833c5148f887b516f2166dda3fbbef7d4ba31bd9
adnesh <dhamangaonadnesh@gmail.com>
2019-03-14 23:08:28 +0530

    two player mode improvement

--- vis.c
+++ vis.c
@@ -35,6 +35,14 @@ void AD_DrawCircle(SDL_Renderer **ren, float center_x_coordinate, float center_y
 	}
 }
 
+void DrawSnakeBody(SDL_Renderer **ren, int center_x, int center_y, int side) {
+	int x = center_x + side;
+	int y1 = center_y - side;
+	int y2 = center_y + side;
+	for(int i = center_x - side; i < x; i++)
+		SDL_RenderDrawLine(*ren, i, y1, i, y2);
+}
+
 extern TTF_Font *arial;
 extern SDL_Rect R1;
 extern SDL_Color white;
@@ -42,6 +50,8 @@ extern SDL_Color white;
 void AD_DrawSnake(SDL_Renderer **ren, snake *s, snake *t, struct XY co, long int score, int tflag) {
 	if(s->dir == ND)
 		return;
+	if(t->dir == ND)
+		return;	
 	SDL_Surface *sur;
 	SDL_Texture *tex_newgame;
 	char scoretext[64];
@@ -57,10 +67,11 @@ void AD_DrawSnake(SDL_Renderer **ren, snake *s, snake *t, struct XY co, long int
 	for(i = 0; i < BLOCKSIZE / s->speed; i++) {
 		SDL_SetRenderDrawColor(*ren, 0, 100, 0, 200);
 		SDL_RenderClear(*ren);
+		SDL_SetRenderDrawColor(*ren, 0, 0, 0, 0);
 		SDL_RenderCopy(*ren, tex_newgame, NULL, &R1);
 		n = s->tail;
 		while(n != s->head) {
-			AD_DrawCircle(ren, n->x, n->y, SNAKENODE, 0, 0, 0, 0);
+			DrawSnakeBody(ren, n->x, n->y, SNAKENODE);
 			switch(n->dir) {
 				case UP:
 					n->y -= s->speed;
@@ -79,7 +90,6 @@ void AD_DrawSnake(SDL_Renderer **ren, snake *s, snake *t, struct XY co, long int
 			}
 			n = n->prev;
 		}
-		n = s->head;
 		AD_DrawCircle(ren, n->x, n->y, BLOCKSIZE * 1.5, 255, 0, 0, 0);
 		switch(s->head->dir) {
 			case UP:
@@ -97,6 +107,46 @@ void AD_DrawSnake(SDL_Renderer **ren, snake *s, snake *t, struct XY co, long int
 			default:
 				break;				
 		}
+		if(tflag) {
+			n = t->tail;
+			while(n != t->head) {
+				DrawSnakeBody(ren, n->x, n->y, SNAKENODE);
+				switch(n->dir) {
+					case UP:
+						n->y -= t->speed;
+						break;
+					case DOWN:
+						n->y += t->speed;
+						break;
+					case RIGHT:
+						n->x += t->speed;
+						break;
+					case LEFT:
+						n->x -= t->speed;
+						break;
+					default:
+						break;				
+				}
+				n = n->prev;
+			}
+			AD_DrawCircle(ren, n->x, n->y, BLOCKSIZE * 1.5, 255, 0, 0, 0);
+			switch(t->head->dir) {
+				case UP:
+					t->head->y -= t->speed;
+					break;
+				case DOWN:
+					t->head->y += t->speed;
+					break;
+				case RIGHT:
+					t->head->x += t->speed;
+					break;
+				case LEFT:
+					t->head->x -= t->speed;
+					break;
+				default:
+					break;				
+			}
+		}
 		MakeBoard(ren);
 		if(co.t == SMALL)
 			AD_DrawCircle(ren, co.x, co.y, BLOCKSIZE, 255, 255, 0, 0);
@@ -107,10 +157,11 @@ void AD_DrawSnake(SDL_Renderer **ren, snake *s, snake *t, struct XY co, long int
 	}
 	SDL_SetRenderDrawColor(*ren, 0, 100, 0, 200);
 	SDL_RenderClear(*ren);
+	SDL_SetRenderDrawColor(*ren, 0, 0, 0, 0);
 	SDL_RenderCopy(*ren, tex_newgame, NULL, &R1);
 	n = s->tail;
 	while(n != s->head) {
-		AD_DrawCircle(ren, n->x, n->y, SNAKENODE, 0, 0, 0, 0);
+		DrawSnakeBody(ren, n->x, n->y, SNAKENODE);
 		switch(n->dir) {
 			case UP:
 				n->y -= BLOCKSIZE % s->speed;
@@ -129,7 +180,6 @@ void AD_DrawSnake(SDL_Renderer **ren, snake *s, snake *t, struct XY co, long int
 		}
 		n = n->prev;
 	}
-	n = s->head;
 	AD_DrawCircle(ren, n->x, n->y, BLOCKSIZE * 1.5, 255, 0, 0, 0);
 	switch(s->head->dir) {
 		case UP:
@@ -147,6 +197,47 @@ void AD_DrawSnake(SDL_Renderer **ren, snake *s, snake *t, struct XY co, long int
 		default:
 			break;				
 	}
+	if(tflag) {
+		n = t->tail;
+		SDL_SetRenderDrawColor(*ren, 0, 0, 0, 0);
+		while(n != t->head) {
+			DrawSnakeBody(ren, n->x, n->y, SNAKENODE);
+			switch(n->dir) {
+				case UP:
+					n->y -= BLOCKSIZE % t->speed;
+					break;
+				case DOWN:
+					n->y += BLOCKSIZE % t->speed;
+					break;
+				case RIGHT:
+					n->x += BLOCKSIZE % t->speed;
+					break;
+				case LEFT:
+					n->x -= BLOCKSIZE % t->speed;
+					break;
+				default:
+					break;				
+			}
+			n = n->prev;
+		}
+		AD_DrawCircle(ren, n->x, n->y, BLOCKSIZE * 1.5, 255, 0, 0, 0);
+		switch(t->head->dir) {
+			case UP:
+				t->head->y -= BLOCKSIZE % t->speed;
+				break;
+			case DOWN:
+				t->head->y += BLOCKSIZE % t->speed;
+				break;
+			case RIGHT:
+				t->head->x += BLOCKSIZE % t->speed;
+				break;
+			case LEFT:
+				t->head->x -= BLOCKSIZE % t->speed;
+				break;
+			default:
+				break;				
+		}
+	}
 	MakeBoard(ren);
 	if(co.t == SMALL)
 		AD_DrawCircle(ren, co.x, co.y, BLOCKSIZE, 255, 255, 0, 0);
@@ -156,7 +247,12 @@ void AD_DrawSnake(SDL_Renderer **ren, snake *s, snake *t, struct XY co, long int
 	SDL_Delay(1000 / 60);
 	for(n = s->tail; n != s->head; n = n->prev) 
 		n->dir = n->prev->dir;	
-	s->head->dir = s->dir;	
+	s->head->dir = s->dir;
+	if(tflag) {
+		for(n = t->tail; n != t->head; n = n->prev)
+			n->dir = n->prev->dir;
+		t->head->dir = t->dir;	
+	}	
 	SDL_DestroyTexture(tex_newgame);
 }
 
